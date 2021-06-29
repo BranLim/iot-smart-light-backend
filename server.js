d99f759797e9b81716916043822c79670cf28a95
Brandon Lim <limws.brandon@gmail.com>
2021-06-29 09:21:57 +0800

    Add database connection

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dbConfig = require("./config/database.config.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Fail to connect to database");
+    process.exit();
+  });
 
 app.get("/api/v1/index", (req, res) => {
   res.json({ message: "Smartlight Backend" });
